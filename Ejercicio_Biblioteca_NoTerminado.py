b5b20a2ef223fefa32b34a965e8a13177d24e94e
neurott <neurott@protonmail.com>
2025-12-06 21:27:45 -0400

    cffff

---
+++ Ejercicio_Biblioteca_NoTerminado.py
@@ -0,0 +1,10 @@
+# Ejercicio: Menú interactivo para agregar libros a la biblioteca. Enunciado:
+# Crea un programa que permita a un usuario gestionar una biblioteca de libros. El programa debe tener un menú con las siguientes opciones:
+# Agregar un libro: El usuario ingresará el título, el autor, el año y el género del libro.
+# Ver todos los libros: El programa mostrará todos los libros en la biblioteca (diccionario).
+# Salir: Termina el programa.
+# El diccionario biblioteca debe almacenar los libros como clave (el título del libro) y los valores como otro diccionario que contenga:
+# Autor
+# Año de publicación
+# Género literario
+biblioteca = {}
